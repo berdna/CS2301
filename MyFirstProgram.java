6d6d716e33d1c73fb246488e99a980f0cba69a06
Andre Barnes <contact@csabarnes.com>
2017-01-18 09:43:24 -0500

    Create MyFirstProgram.java

---
+++ MyFirstProgram.java
@@ -0,0 +1,6 @@
+public class MyFirstProgram {
+  public static void main (String[] args) {
+    System.out.println("This is my first Java Program!")
+    System.out.println("It does not do much " ) 
+    }
+ }
