c3e100941f2889609f5abff5841238ed9bb04096
Andre Barnes <contact@csabarnes.com>
2017-01-20 11:11:40 -0500

    Create Practice_1_4.java
    Math functions to find area and perimeter of a rectangle

---
+++ Practice_1_4.java
@@ -0,0 +1,9 @@
+public class Practice_1_4 {
+  public static void main (String[] args) {
+  int width = 4;
+  int height = 8;
+    System.out.println("The area of the data set is " +(width * height));
+    System.out.println("The perimeter of the rectangle is " + (2*width + 2*height));
+  }
+}
+    
