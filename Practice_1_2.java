865b7ac1a31c5148a61f0310b568a4d0c942be4f
Andre Barnes <contact@csabarnes.com>
2017-01-20 11:57:08 -0500

    Practice_1_2_Diamond
    Can update to for loop which is best method

--- Practice_1_2.java
+++ Practice_1_2.java
@@ -1,21 +1,12 @@
 public class Practice_1_2 {
   public static void main (String[] args) {
-    System.out.println("*");
-    System.out.print("*");
-    System.out.println("*");
-    System.out.print("*");
-    System.out.print("*");
-    System.out.println("*");
-    System.out.print("*");
-    System.out.print("*");
-    System.out.print("*");
-    System.out.println("*");
-    System.out.print("*");
-    System.out.print("*");
-    System.out.println("*");
-    System.out.print("*");
-    System.out.println("*");
-    System.out.print("*");
+    System.out.println("   * ");
+    System.out.println("  * *");
+    System.out.println(" * * *");
+    System.out.println("* * * * ");
+    System.out.println(" * * *");
+    System.out.println("  * *");
+    System.out.print("   *");
     }
 }
    
