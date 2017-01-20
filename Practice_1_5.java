9111c012dc169cd405f4eb68214d28bcee156ee6
Andre Barnes <contact@csabarnes.com>
2017-01-20 11:27:54 -0500

    Create Practice_1_5.java

---
+++ Practice_1_5.java
@@ -0,0 +1,13 @@
+public class Practice_1_5 {
+  public static void main(String[] args){
+    
+    //The car is traveling 75 miles in 50 minutes
+    //The rate would be 90 miles an hour or 1.5 miles per minute
+    //1 Mile is equal to 1.6 kilometers
+    int mph = 90;
+    double k = 1.6;
+    double kilo = mph * k;
+    System.out.println("This car is traveling at " + kilo + " Kilometers per hour");
+    }
+}
+    
