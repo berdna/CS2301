5cecc7ed0972e32866283e585d2a72a7c6ebe768
Andre Barnes <contact@csabarnes.com>
2017-01-20 09:22:37 -0500

    Addition_v.0.1.1.java
    Added comments to define concatenation and summation of two values

--- addition.java
+++ addition.java
@@ -3,6 +3,6 @@ public class Addition
        public static void main (String[] args) 
       { 
          System.out.println ("24 and 45 concatenated: " + 24 + 45); // Prints out 2445 just attaching the two values together
-         System.out.println ("24 and 45 added: " + (24 + 45)); // Prints out the sum of 24 and 45 which is 69
+         System.out.println ("24 and 45 added: " + (24 + 45)); // Prints out the sum of 24 and 45 which is 69 
       } 
    } 
