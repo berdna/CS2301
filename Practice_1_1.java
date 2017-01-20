b58f6e033e20dbfe03a4100bbb5cb9313015c9bc
Andre Barnes <contact@csabarnes.com>
2017-01-20 09:46:42 -0500

    Practice_1_1.java
    Prints lines of information

---
+++ Practice_1_1.java
@@ -0,0 +1,14 @@
+
+public class Practice_1_1 
+{
+  public static void main (String[] args) 
+  {
+    System.out.println("My name is :" + " Andre");
+    System.out.println("My birthday is :" + " April 21, 1997");
+    System.out.println("My hobby is :" + " backend development");
+    System.out.println("My favorite book is :" + " The 7 Habits of the Highly Effective");
+    System.out.println("My favorite movie is :" + " Titanic");
+    }
+}
+    
+   
