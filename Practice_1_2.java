a8dd79f933061bde2e44e5ad793c9d7afe95b0dc
Andre Barnes <contact@csabarnes.com>
2017-01-20 09:52:49 -0500

    Create Practice_1_2.java
    Very shitty iteration of a diamond, should probably use a for loop

---
+++ Practice_1_2.java
@@ -0,0 +1,21 @@
+public class Practice_1_2 {
+  public static void main (String[] args) {
+    System.out.println("*");
+    System.out.print("*");
+    System.out.println("*");
+    System.out.print("*");
+    System.out.print("*");
+    System.out.println("*");
+    System.out.print("*");
+    System.out.print("*");
+    System.out.print("*");
+    System.out.println("*");
+    System.out.print("*");
+    System.out.print("*");
+    System.out.println("*");
+    System.out.print("*");
+    System.out.println("*");
+    System.out.print("*");
+    }
+}
+   
